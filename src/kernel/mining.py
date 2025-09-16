ea763ab5dd947cf5e91fb6123644a4020fadf008
Andrii-sat <158034288+Andrii-sat@users.noreply.github.com>
2025-09-17 00:13:14 +0800

    Update mining.py

--- src/kernel/mining.py
+++ src/kernel/mining.py
@@ -1,1 +1,9 @@
+from typing import Optional
+from .blockchain import Chain, Block
 
+class Miner:
+    def __init__(self, chain: Chain):
+        self.chain = chain
+
+    def mine_once(self, max_iters: int = 200000, difficulty: int = None) -> Optional[Block]:
+        return self.chain.try_mine(max_iters=max_iters, difficulty=difficulty)
